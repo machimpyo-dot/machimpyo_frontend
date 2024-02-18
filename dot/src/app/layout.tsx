6fef578920e919c557f791cd231270cd67ea893f
nakyoung98 <jay3925@cau.ac.kr>
2024-02-18 21:35:01 +0900

    Feat: "기본 Next.js 프로젝트 구성"
    불필요한 파일 삭제

---
+++ dot/src/app/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="kr">
+      <body>{children}</body>
+    </html>
+  );
+}
